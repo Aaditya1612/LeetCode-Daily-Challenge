5288fdfb88e36c7ac7959003db87d7019b8e00d1
Aaditya Raj <aadityaraj085@gmail.com>
2022-11-06 11:26:53 +0530

    Create solution.java

---
+++ November 2022/899. Orderly Queue/solution.java
@@ -0,0 +1,20 @@
+// Asked on: 06-11-2022 (DD-MM-YYYY)
+
+class Solution {
+  public String orderlyQueue(String s, int k) {
+    if (k > 1) {
+      char tempArray[] = s.toCharArray();
+      Arrays.sort(tempArray);
+      return new String(tempArray);
+    } else {
+      String ans = s;
+      for (int i = 0; i < s.length(); i++) {
+        s = s.substring(1) + s.substring(0, 1);
+        if (ans.compareTo(s) > 0) {
+          ans = s;
+        }
+      }
+      return ans;
+    }
+  }
+}
