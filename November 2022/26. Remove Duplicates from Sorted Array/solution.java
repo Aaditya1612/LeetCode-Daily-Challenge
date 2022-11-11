9959a13e6127aa3a1bc976351b76aa44f68688a9
Aaditya Raj <aadityaraj085@gmail.com>
2022-11-11 21:09:24 +0530

    Update solution.java

--- November 2022/26. Remove Duplicates from Sorted Array/solution.java
+++ November 2022/26. Remove Duplicates from Sorted Array/solution.java
@@ -2,13 +2,17 @@
 // Language: Java
 
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        
-        for(int i: nums){
-            set.add(i);
-        }
-        
-        return set.size();
-    }
+   public int removeDuplicates(int[] nums) {
+		if (nums.length == 0 || nums.length == 1) {
+			return nums.length;
+		}
+		int j = 0;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] != nums[i + 1]) {
+				nums[j++] = nums[i];
+			}
+		}
+		nums[j++] = nums[nums.length - 1];
+		return j;
+	}
 }
