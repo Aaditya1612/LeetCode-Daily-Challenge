58ed5715fcda4f6dbb57c73a3c7b6f30ffcd127e
Aaditya Raj <aadityaraj085@gmail.com>
2022-12-14 10:08:40 +0530

    Create solution.java

---
+++ December 2022/198. House Robber/solution.java
@@ -0,0 +1,23 @@
+// Asked on 14-12-2022 (DD-MM-YYYY)
+// Language: Java
+
+class Solution {
+    public int rob(int[] nums) {
+        
+        int n = nums.length;
+        int dp[] = new int[n];
+        dp[0]=nums[0];
+        if(n==1){
+            return nums[0];
+        }
+        
+        dp[1] = Math.max(nums[0],nums[1]);
+        
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        
+        return dp[n-1];
+
+    }
+}
